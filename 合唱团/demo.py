3fb5f609604ce6fd189784a8c1c12990088e2a16
quan <1051576073@qq.com>
2018-09-09 21:03:29 +0800

    无关更新

--- 合唱团/demo.py
+++ 合唱团/demo.py
@@ -21,76 +21,28 @@
 -41 -5 -10 -31 -44 -16 -3 -33 -34 -35 -44 -44 -25 -48 -16 -32 -37 -8 -33 -30 -6 -18 -26 -37 -40 -30 -50 -32 -5 -41 -32 -12 -33 -22 -14 -34 -1 -41 -45 -8 -39 -27 -23 -45 -10 -50 -34 
 6 3
 """
-def maxs(stu,d,ch):
-    m = -51
-    ind = -1
-
-    for i in ch:
-
-        l = i-d
-        h = i+d+1
-        if l<0:
-            l=0
-        if h>len(stu):
-            h = len(stu)
-        for s in range(l,h):
-            if stu[s]>m and s not in ch:
-                ind = s
-                m =stu[s]
-
-    return m,ind
-
-
-def mins(stu, d, ch):
-    m = 51
-    ind = -1
-
-    for i in ch:
-        l = i - d
-        h = i + d + 1
-        if l < 0:
-            l = 0
-        if h > len(stu):
-            h = len(stu)
-
-        for s in range(l, h):
-            if stu[s] < m and s not in ch:
-                ind = s
-                m = stu[s]
-
-    return m, ind
 
 n = int(input())
 stu = list(map(int,input().split()))
 k,d = map(int,input().split())
-choice = []
-dchoice = []
-m = max(stu)
-mids = stu.index(m)
-n = min(stu)
-nids = stu.index(m)
-choice.append(mids)
-dchoice.append(nids)
-sum = m
-dsum = n
-for i in range(1,k):
-    m, mids = maxs(stu, d, choice)
-    n, nids = mins(stu,d, dchoice)
+from copy import deepcopy
 
-    if m*n>0:
-        if abs(m)>abs(n):
-            n = m
-            nids = mids
-        else:
-            m = n
-            mids = nids
+row = [0]*n
+dmax = []
+dmin = []
+for i in range(0,k):
+    dmax.append(deepcopy(row))
+    dmin.append(deepcopy(row))
+for nk in range(0,n):
+    dmax[0][nk] = stu[nk]
+    dmin[0][nk] = stu[nk]
 
-    choice.append(mids)
-    dchoice.append(nids)
-    l = [sum*m,sum*n,dsum*m,dsum*n]
-    print(m,mids,n,nids)
-    sum = max(l)
-    dsum = min(l)
-    print(sum,dsum)
+for i in range(0,n):
+    for j in range(1,k):
+        for kd in range(max(0,i-d),i):
+            dmax[j][i] = max(dmax[j][i], dmax[j-1][kd] * stu[i], dmin[j-1][kd] * stu[i])
+            dmin[j][i] = min(dmin[j][i], dmax[j-1][kd] * stu[i], dmin[j-1][kd] * stu[i])
+            print(dmax[j])
+            print(dmin[j])
 
-print(sum)
+print(max(dmax[k-1]))
